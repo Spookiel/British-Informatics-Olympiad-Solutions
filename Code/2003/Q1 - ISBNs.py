c6fd3238463d248edb02e751d18ca6a2cc4cdbef
Luke M <luke.moran2004@gmail.com>
2020-04-16 10:01:11 +0100

    Added  2003 Q1 Analysis

--- Code/2003/Q1 - ISBNs.py
+++ Code/2003/Q1 - ISBNs.py
@@ -3,24 +3,27 @@ d = {str(i):i for i in range(10)}
 d["X"] = 10
 d["10"] = "X"
 
+
 def score(s):
     total = 0
-
     for i in range(len(s)):
         total += (10-i)*d[s[i]]
     #print(total, total%11==0)
     return total%11==0
 
+
 pos = s.index("?")
 
 for p in range(11):
     s[pos] = str(d[str(p)])
-    #print("".join(s))
+    # print("".join(s))
     if score(s):
         print(d[str(p)])
         break
 
 """SCORES FULL CREDIT 24/24
-Watch the problem with 
+Watch the problem with using 10 as an X and vice versa
+We can just try every digit as the question mark, until we find one that is valid
+There are only ten digits, so this runs very quickly
 
 """
--- Code/2004/Q1 - Mayan Calender.py
+++ Code/2004/Q1 - Mayan Calender.py
@@ -1,21 +1,28 @@
 from datetime import timedelta,date
 baseDate = list(map(int,"13 20 7 16 3".split()))[::-1]
 realDate = list(map(int, input("ENTER DATE: ").split()))[::-1]
-janDate = date(2000,1,1)
+janDate = date(2000, 1, 1)
+
+
 def convert(baseDate, realDate):
-    dayList = [20,18,20,20] #Keeps track of how many kins in a uinal etc
-    cumSum = [1,20, 360, 7200, 144000] #Cumulative sum of the days in each part of the calender, eg 144000 days in a Baktun
-    ansDate = [0,0,0,0,0] #Empty list to keep track of the new date
+    dayList = [20, 18, 20, 20] #Keeps track of how many kins in a uinal etc
+    cumSum = [1, 20, 360, 7200, 144000] #Cumulative sum of the days in each part of the calender, eg 144000 days in a Baktun
+    ansDate = [0, 0, 0, 0, 0] #Empty list to keep track of the new date
+
     for i in range(len(realDate)): #We're doing a subtraction, treating each element of a the calender as a digit so to speak
         if baseDate[i] > realDate[i]: #Keeps everything positive
             realDate[i+1] -= 1
             realDate[i] += dayList[i]
         ansDate[i] = realDate[i]-baseDate[i]
+
+
     totDays = sum([ansDate[i]*cumSum[i] for i in range(len(ansDate))]) #Works out the total number of days in our date difference
     finalDate = janDate+timedelta(days=totDays) #Uses Python's Datetime library to skip forward the correct number of days
     finalDate =str(finalDate).split("-")[::-1] #Gets date
     finalDate = [i if int(i) >= 10 else i[1:] for i in finalDate] #Formats date
     print(" ".join(finalDate))
+
+
 convert(baseDate, realDate)
 
 #b)13 20 7 17 14 , 13 20 8 16 9
@@ -37,7 +44,7 @@ endDate = date(4772, 10, 12)
 print(endDate.weekday()) #0 is Monday, 6 is Sunday
 #Gives answer as 3 or Thursday"""
 
-"""
+""" Analysis
 Solution scores 29/29
 This solution uses Python's Datetime library to get full credit very easily.
 You don't even have to know the library off by heart - You can look at the docs during the exam.
