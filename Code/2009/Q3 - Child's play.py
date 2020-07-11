493d2419ec6850b13b7914952947de80f90bb8b3
Spookiel <Luke.moran2004@gmail.com>
2020-07-11 13:15:25 +0100

    Added 2009 Q3 - Child's Play

--- Code/2009/Q3 - Child's play.py
+++ Code/2009/Q3 - Child's play.py
@@ -45,3 +45,61 @@ def b():
 They are the same
 
 """
+
+
+
+"""Explanation:
+
+This is a problem which can be solved using memoized recursion or dynamic programming. In this solution, I chose to use
+
+a recursive function in order to better demonstrate the recurrence relation.
+
+In this problem, to calculate the ways of arranging the blocks with a score of S
+is the number of ways to arrange the blocks of 1 + score(S-1), 2 + score(S-2) ... S + score(0)
+
+where score(0) = 1
+and score(1) = 1
+
+then all that remains is to iteratively build up the answer by trying each digit 1-9 as the next in the answer
+if the ways of arranging that new sum is greater or equal to n, then that answer must be a prefix of it. For example
+
+Say the target score is 4, and the arrangement we are looking for is 5.
+
+Currently our answer is nothing
+So we try starting answer with 1
+We then call score(4-1) = score(3)
+score(3) = 4
+We are looking for the 5th arrangement 
+5 > 4
+
+n = 5 - 4
+n = 1
+
+now we try the digit 2
+score(2) = 2
+2 >= 1
+
+so n stays as 1
+and our answer is now 2 1
+
+then we try 1 again
+
+score(1) = 1
+
+answer is 2 1 1
+
+Now that the sum of answer is equal to the target sum, we stop
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
