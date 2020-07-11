8b7ca1ebf16ab87dc78662081c2be60b2de713a3
Spookiel <Luke.moran2004@gmail.com>
2020-07-11 17:15:08 +0100

    Added 2011 Q1, 2010 Q1-3

---
+++ Code/2011/Q1 - Fibonacci letters.py
@@ -0,0 +1,30 @@
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+let1, let2, n = input().split()
+n = int(n)
+
+let1 = alpha.index(let1)+1
+let2 = alpha.index(let2)+1
+
+for i in range(n-2):
+    let1,let2 = let2, (let1+let2)%26
+
+if n==1:
+    print(alpha[let1-1])
+else:
+
+    print(alpha[let2-1])
+
+def b():
+    a = alpha.index("X")-alpha.index("F")
+    print(alpha[a])
+    c = (alpha.index("H")-alpha.index("Q"))%26
+    print(alpha[c])
+
+from numpy.linalg import matrix_power
+
+def c(num):
+    pass
+
+#b) S, R
+
