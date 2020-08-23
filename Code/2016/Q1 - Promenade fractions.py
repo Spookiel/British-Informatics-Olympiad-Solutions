9e2198c1b1916fc1bf7c8ce02cf5b8cf6cb9c02d
Luke Moran <luke.moran2004@gmail.com>
2020-08-23 15:18:16 +0100

    Added 2016 Q1

--- Code/2016/Q1 - Promenade fractions.py
+++ Code/2016/Q1 - Promenade fractions.py
@@ -1,2 +1,24 @@
 
+def solve(inp):
+    left = [1,0]
+    right = [0,1]
 
+    for i in inp:
+        if i=="L":
+            left = [left[0]+right[0], left[1]+right[1]]
+        else:
+            right = [left[0]+right[0], left[1]+right[1]]
+        #print(left, right)
+
+    ans = [left[0]+right[0], left[1]+right[1]]
+    return f"{ans[0]} / {ans[1]}"
+
+print(solve(input()))
+
+#b() -> LRRR
+
+def c():
+    for i in range(1,10):
+        print(solve("L"*i), i)
+
+#c() -> 999,999
