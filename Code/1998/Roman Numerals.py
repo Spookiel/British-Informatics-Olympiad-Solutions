172aa103f4a76c74787e909285dac631aa1d001e
Lawson Lam <109973600+Catatria@users.noreply.github.com>
2022-08-05 08:19:13 +0800

    Create Roman Numerals.py

---
+++ Code/1998/Roman Numerals.py
@@ -0,0 +1,35 @@
+sep = []
+ans = []
+num = str(input("Input a number. "))
+
+if 1 > num >= 4000:
+  print("Error: Invalid number, above 4000 or below 1.")
+  exit()
+
+for i in range(4-len(num)):
+  sep.append(0)
+for i in range(len(num)):
+  sep.append(int(num[i]))
+
+romanNumerals = [["C", "D", "M"], ["X", "L", "C"], ["I", "V", "X"]]
+
+for i in range(sep[0]):
+  ans.append("M")
+
+for i in range(3):
+  if 3 >= sep[i+1] >= 1:
+    for j in range(sep[i+1]):
+      ans.append(romanNumerals[i][0])
+  elif sep[i+1] == 4:
+    ans.append(romanNumerals[i][0]+romanNumerals[i][1])
+  elif sep[i+1] == 5:
+    ans.append(romanNumerals[i][1])
+  elif 8 >= sep[i+1] >= 6:
+    ans.append(romanNumerals[i][1])
+    for j in range(sep[i+1]-5):
+      ans.append(romanNumerals[i][0])
+  elif sep[i+1] == 9:
+    ans.append(romanNumerals[i][0]+romanNumerals[i][2])
+    
+ans = "".join(ans)
+print("\n"+ans)
