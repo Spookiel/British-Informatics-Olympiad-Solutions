57773729fefea141eb1e4566f4304d578d69e139
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-09-29 18:15:33 +0100

    Update Q1 - Lojban.py

--- Code/2002/Q1 - Lojban.py
+++ Code/2002/Q1 - Lojban.py
@@ -1,39 +1,16 @@
-with open("Q1.in", "r") as fin:
-    d = {}
-    rd = {}
-    for i in range(3):
-        lines = fin.readline().split()
-        pairs = [lines[i:i+2] for i in range(0, len(lines), 2)]
-        for p in pairs:
-            a,b = p
-            d[int(a)] = b
-            rd[b] = a
+#Solution by @Pararcana
+denary = []
+lojbanDict = {
+  "no": "0", "pa": "1",
+  "re": "2", "ci": "3",
+  "vo": "4", "mu": "5", 
+  "xa": "6", "ze": "7",
+  "bi": "8", "so": "9"
+}
 
-def convertTo(num):
-    ans = ""
-    for i in str(num):
-        ans += d[int(i)]
-    return ans
+lojban = input("Write a number in lojiban: ")
 
-def convertFrom(string):
-    pairs = "".join([rd[string[i:i+2]] for i in range(0, len(string), 2)])
-    return int(pairs)
+for i in range(0, len(lojban), 2):
+  denary.append(lojbanDict[lojban[i:i+2]])
 
-print(convertFrom(input("ENTER NUMBER: ")))
-
-#1b)
-#print(convertTo(convertFrom("sovo")+convertFrom("rexa")))
-# = pareno
-
-
-""" Part 1c) - vobi
-
-def sumAlpha(string):
-    return sum([ord(i)-96 for i in string])
-for i in range(1,1000):
-    if sumAlpha(convertTo(i))==i:
-        print(convertTo(i), i)
-        break"""
-
-
-"""SCORES FULL CREDIT 28/28"""
+print("".join(denary))
