7b907e077d9e7e7294bf043a0e92a1af07293392
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-09-30 07:48:58 +0100

    Update Q2 - Decoder Ring.py
    Fixed underflow error in previous program.

--- Code/2018/Q2 - Decoder Ring.py
+++ Code/2018/Q2 - Decoder Ring.py
@@ -1,19 +1,21 @@
 #Solution by @Pararcana
 alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-encryptor, answer = [], []
+ring, answer = [], []
 counter = 0
 
-n = input("Input a number and word. ").split()
+number, word = input("Input a number and word. ").split()
+number, word = int(number), word.upper()
 
 for _ in range(26):
-  counter += int(n[0]) <= 26 and int(n[0])-1 or (int(n[0]) % len(alphabet)) - 1
+  counter += (number % len(alphabet)) - 1
   if counter >= len(alphabet):
     counter %= len(alphabet)
-  encryptor.append(alphabet.pop(counter))
-print("".join(encryptor[0:6]))
+  elif counter == -1:
+    counter = len(alphabet) - 1
+  ring.append(alphabet.pop(counter))
+print("".join(ring[0:6]))
 
-for i in range(len(n[1])):
-  answer.append(encryptor[ord(n[1][i])-65])
-  encryptor.append(encryptor.pop(0))
-  
+for i in range(len(word)):
+  answer.append(ring[ord(word[i])-65])
+  ring.append(ring.pop(0))
 print("".join(answer))
