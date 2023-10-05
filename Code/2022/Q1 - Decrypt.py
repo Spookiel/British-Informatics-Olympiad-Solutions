37ea8d87a2a608ff0b2e8815099d1722048488da
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-05 21:39:18 +0100

    Update Q1 - Decrypt.py

--- Code/2022/Q1 - Decrypt.py
+++ Code/2022/Q1 - Decrypt.py
@@ -26,6 +26,41 @@ else:
   exit()
 
 print("".join(decrypt(s)))
+
+def b():
+    for word in product(alpha, repeat=5):
+        sword = list(word)
+        #print(sword, decrypt(sword))
+        if sword == decrypt(sword):
+            print("Part b:", sword)
+            return
+
+def c():
+    ### Encrypting until the same is the same as decrypting until the same
+
+    print(find_cyle("OLYMPIAD"))
+
+def find_cyle(s):
+    c = 0
+    orig = list(s)
+    while c < 500:
+        s = decrypt(s)
+        #print(s, c)
+        if s == orig:
+            return c
+        c += 1
+
+def d():
+
+    t = 0
+    for s in product(alpha, repeat=3):
+        clen = find_cyle(s)
+        clen += 1
+        if  999999999999%clen == 0:
+            #print(s, clen)
+            t += 1
+    print(t)
+  
 #b() # z z z z a
 c() # 103
 d()
