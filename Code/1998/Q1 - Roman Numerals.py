c71d56747bec2df1494e3edd21b30a743813b806
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-05 20:29:23 +0100

    Update Q1 - Roman Numerals.py

--- Code/1998/Q1 - Roman Numerals.py
+++ Code/1998/Q1 - Roman Numerals.py
@@ -2,15 +2,14 @@
 num = "{:04d}".format(int(input("Please input a number: ")))
 ans = []
 roman = [
-["M"],
-["C", "D", "M"],
-["X", "L", "C"],
-["I", "V", "X"]
+["M", ""],
+["C", "D", "M", ""],
+["X", "L", "C", ""],
+["I", "V", "X", ""]
 ]
-format = [[0], [0,0], [0,0,0], [0,1], [1], [1,0], [1,0,0], [1,0,0,0], [0,2]]
+format = [[-1], [0], [0,0], [0,0,0], [0,1], [1], [1,0], [1,0,0], [1,0,0,0], [0,2]]
 for i, n in enumerate(num):
-  if n != "0":
-    for j in range(len(format[int(n)-1])):
-       ans.append(roman[i][format[int(n)-1][j]])
-
-print("".join(ans))
+    for j in range(len(format[int(n)])):
+        ans.append(roman[i][format[int(n)][j]])
+    
+return "".join(ans)
