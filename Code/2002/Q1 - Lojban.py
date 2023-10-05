98c2f2428c41ba61b85337555f86e293c8accc32
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-05 21:36:19 +0100

    Update Q1 - Lojban.py
    added back parts b and c

--- Code/2002/Q1 - Lojban.py
+++ Code/2002/Q1 - Lojban.py
@@ -14,3 +14,17 @@ for i in range(0, len(lojban), 2):
   denary.append(lojbanDict[lojban[i:i+2]])
 
 print("".join(denary))
+
+#1b)
+#print(convertTo(convertFrom("sovo")+convertFrom("rexa")))
+# = pareno
+
+
+# Part 1c)
+# def sumAlpha(string):
+#     return sum([ord(i)-96 for i in string])
+# for i in range(1,1000):
+#     if sumAlpha(convertTo(i))==i:
+#         print(convertTo(i), i)
+#         break
+# = vobi
