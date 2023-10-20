43a4d554ac4f1d71f600154ea3276f6ece0ef036
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-20 18:36:48 +0100

    Update Q1 - Watch the clock.py

--- Code/2013/Q1 - Watch the clock.py
+++ Code/2013/Q1 - Watch the clock.py
@@ -1,34 +1,18 @@
+#Solution by @Pararcana
+aIncre, bIncre = (int(x) + 60 for x in input("Enter two numbers: ").split())
+a, b = aIncre, bIncre
 
+def minutesToHours(min):
+  return [(min // 60) % 24, min % 60]
 
-n,m = list(map(int, input().split()))
+while minutesToHours(a) != minutesToHours(b):
+  a += aIncre
+  b += bIncre
 
+hours, minutes = minutesToHours(a)
+print("{:02d}".format(hours)+":"+"{:02d}".format(minutes))
 
-def solve(n,m, debug=False):
-    c1 = [0,0]
-    c2 = [0,0]
-    tot = 0
-    while True:
-        c1[1] += n
-        c2[1] += m
-        c1[0] += c1[1]//60
-        c1[1] %= 60
-        c1[0] += 1
-        c1[0] %= 24
-        c2[0] += c2[1]//60
-        c2[1] %= 60
-        c2[0] += 1
-        c2[0]%=24
-        tot += 1
-        if c1==c2:
-            break
-    ans = [("0" if len(str(c1[0])) < 2 else "")+str(c1[0]), ("0" if len(str(c1[1])) < 2 else "")+str(c1[1])]
-    if not debug:
-        print(":".join(ans))
-    return c1, tot
-
-solve(n,m)
-
-
+#Rest by Spookiel
 def b():
     for i in range(20):
         print(solve(0, i,debug=True), i)
